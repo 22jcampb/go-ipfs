d39bdf48d7701321ddc77740f5534f5ef4e6d7ca
22jcampb <85803754+22jcampb@users.noreply.github.com>
2022-04-03 15:13:59 -0400

    Update addresses.go
    Changed grammar slightly

--- config/addresses.go
+++ config/addresses.go
@@ -5,7 +5,7 @@ type Addresses struct {
 	Swarm          []string // addresses for the swarm to listen on
 	Announce       []string // swarm addresses to announce to the network, if len > 0 replaces auto detected addresses
 	AppendAnnounce []string // similar to Announce but doesn't overwrite auto detected addresses, they are just appended
-	NoAnnounce     []string // swarm addresses not to announce to the network
+	NoAnnounce     []string // swarm addresses IPFS will not announce to the network
 	API            Strings  // address for the local API (RPC)
-	Gateway        Strings  // address to listen on for IPFS HTTP object gateway
+	Gateway        Strings  // address for IPFS HTTP object gateway to listen on
 }
